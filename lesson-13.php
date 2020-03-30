d810e488f09f27cfea7b3d04e40bc4c8f82e420a
mayowDev <dinnod4@gmail.com>
2020-03-30 06:53:13 +0500

    variables, functions

---
+++ lesson-13.php
@@ -0,0 +1,26 @@
+<?php 
+
+	//include('ninjass.php');
+	
+	//require('ninjass.php'); // require results fatal error
+
+	//include 'ninjas.php'; // include cariers the error
+
+	//require 'ninjas.php';
+
+	//echo 'end of php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Tutorials</title>
+</head>
+<body>
+
+	<?php include('content.php'); ?>
+	<?php include('content.php'); ?>
+
+</body>
+</html>

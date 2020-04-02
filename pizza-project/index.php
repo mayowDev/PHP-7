d9069677914362156af5417aba6884c21124e675
mayowDev <dinnod4@gmail.com>
2020-04-02 23:39:43 +0500

    connect to database using mysqli

--- pizza-project/index.php
+++ pizza-project/index.php
@@ -1,5 +1,12 @@
 <?php 
 
+    // connect to the database usign mysqli
+    $conn = mysqli_connect('localhost', 'mayow', 'test1234', 'pizza_hat');
+    // check connection
+	if(!$conn){
+		echo 'Connection error: '. mysqli_connect_error();
+	}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">

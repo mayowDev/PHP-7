0f2196373595377968029d149a1e286509d3fbca
mayowDev <dinnod4@gmail.com>
2020-04-06 23:01:26 +0500

    L-26 Get data from database

--- pizza-project/index.php
+++ pizza-project/index.php
@@ -1,11 +1,27 @@
 <?php 
 
     // connect to the database usign mysqli
-    $conn = mysqli_connect('localhost', 'mayow', 'test1234', 'pizza_hat');
+    $conn = mysqli_connect('localhost', 'ninja', 'test123', 'pizza-hat');
     // check connection
 	if(!$conn){
 		echo 'Connection error: '. mysqli_connect_error();
-	}
+    }
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
 
 ?>
 <!DOCTYPE html>

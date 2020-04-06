9930798645bfd1b9f85828307e3a7c9eb7faf04e
mayowDev <dinnod4@gmail.com>
2020-04-06 23:04:56 +0500

    L-27&28&29 Render data to browser, using exlode function, the flow syntax

--- pizza-project/index.php
+++ pizza-project/index.php
@@ -28,6 +28,34 @@
 <html lang="en">
     
     <?php require('templates/header.php')?>
+    <h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza): ?>
+
+				<div class="col s6 m4">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<ul class="grey-text">
+								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+									<li><?php echo htmlspecialchars($ing); ?></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php endforeach; ?>
+
+		</div>
+	</div>
+
     <?php require('templates/footer.php')?>
     
 </body>

a0676a5f00d1ba96a8269e121c9d157f3588ff54
mayowDev <dinnod4@gmail.com>
2020-04-07 05:05:36 +0500

    L-32&33 delete single record and add image

--- pizza-project/components/header.php
+++ pizza-project/components/header.php
@@ -9,6 +9,13 @@
   	.brand-text{
   		color: #cbb09c !important;
   	}
+	.pizza{
+		width: 100px;
+		margin: 40px auto -30px;
+		display: block;
+		position: relative;
+		top: -30px;
+		}
     form{
   		max-width: 460px;
   		margin: 20px auto;
